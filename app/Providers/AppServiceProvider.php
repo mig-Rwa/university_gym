da146c98193cb0d762ce6b135795dd03773634cd
mig-Rwa <miguelmbabatunga32@gmail.com>
2025-06-10 11:13:21 +0300

    Force HTTPS scheme in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    public function boot()
     {
-        //
+        if ($this->app->environment('production')) {
+            // Force all URLs (assets, routes, etc.) to use HTTPS
+            URL::forceScheme('https');
+        }
     }
 }
